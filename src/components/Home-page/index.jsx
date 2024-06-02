c0c310991ade1ecbc6179a7cef8043aa099b3650
munisaturgunova <munisaturgunova07@gmail.com>
2024-06-03 01:19:17 +0500

    commit

--- src/components/Home-page/index.jsx
+++ src/components/Home-page/index.jsx
@@ -1,7 +1,5 @@
 import Showcase from "../Showcase";
 import Layout from "../Layout";
-import { Title1 } from "../Typography";
-import { Paragraph1 } from "../Typography";
 import clsx from "clsx";
 import cn from "./style.module.scss";
 
@@ -11,12 +9,6 @@ function HomePage() {
     <div className={clsx(cn["home-page"])}>
        <Showcase/>
        <Layout/>
-       <div className={clsx(cn["section1"])}>
-            <div className={clsx(cn["section1_div1"])}>
-                
-            </div>
-       </div>
-
     </div>
   );
 }
