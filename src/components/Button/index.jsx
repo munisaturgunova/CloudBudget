1a4f1dbaac2b313c553eb55fff17c5ede0bcd97c
munisaturgunova <munisaturgunova07@gmail.com>
2024-06-02 16:44:49 +0500

    create button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+import cn from "./style.module.scss";
+
+export const PrimaryButton = (props) => {
+  return (
+    <button className= {clsx(cn["primary-button"])} {...props}>
+      {props.children}
+    </button>
+  );
+};
+
+export const SecondaryButton = (props) => {
+  return (
+    <button className={clsx(cn["secondary-button"])} {...props}>
+      {props.children}
+    </button>
+  );
+};
