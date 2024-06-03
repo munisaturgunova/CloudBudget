2cddb9fc492f29b14d5180906cd93ff697c104dc
munisaturgunova <munisaturgunova07@gmail.com>
2024-06-03 13:02:35 +0500

    create img

--- src/components/images/index.jsx
+++ src/components/images/index.jsx
@@ -1,14 +1,11 @@
 import clsx from "clsx";
 import cn from "./style.module.scss";
 
-const ImageDisplay = ({ imageUrl }) => {
-  return (
+function Img ({url}) {
+  return(
     <div className= {clsx(cn["img"])}>
-      <img src={imageUrl} alt="Displayed Image" />
+      <img src={url} alt="Displayed Image" />
     </div>
-  );
-};
-
-export const displayImage = (imageUrl) => {
-  return <ImageDisplay imageUrl={imageUrl} />;
-};
+  )
+}
+export default Img;
